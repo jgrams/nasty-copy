706eff1dcfe3ed499943eb18806022cfaf5392a0
jgrams <tynanjk@gmail.com>
2019-09-25 23:14:27 -0500

    rearragned app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import RegisterInterest from './components/RegisterInterest';
+import InteractivePanel from './components/InteractivePanel';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Welcome. Has someone ever been mean to you?
-        </p>
+        <InteractivePanel />
         <RegisterInterest />
       </header>
     </div>
