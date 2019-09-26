149caa23f0cf4888ec24febcddbc3213a7d31b85
jgrams <tynanjk@gmail.com>
2019-09-25 21:40:44 -0500

    intrest text and component

---
+++ src/components/RegisterInterest.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RegisterInterest extends React.Component {
+	render() {
+		return (
+			<div className='submit'>
+				<h1>I want to humiliate my enemies.</h1>
+				<h7>And I'm willing to share personal information.</h7>
+				<textfield></textfield>
+			</div>
+		)
+	}
+}
+
+export default RegisterInterest;
