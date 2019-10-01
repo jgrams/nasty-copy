26d21e7286da97092768e4322e59e0301067ab41
jgrams <tynanjk@gmail.com>
2019-09-30 21:22:48 -0500

    added card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,17 @@ import React from 'react';
 class Card extends React.Component {
 	render() {
 		return(
-			<div className="card">
-				<button className={this.props.color + ' card-button'}>
-					{this.props.text}
-				</button>
+			<div className='card-container'>
+				<div className="card">
+					<div className={this.props.color + " card-header"}>
+						<text className="card-header-text">
+							{this.props.headerText}
+						</text>
+					</div>
+					<button className={this.props.color + ' card-button'}>
+						{this.props.text}
+					</button>
+				</div>
 			</div>
 		)
 	}
