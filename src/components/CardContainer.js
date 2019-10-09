e7f69556df3c1b90e9a51cb16b563f03334f70d0
jgrams <tynanjk@gmail.com>
2019-10-09 10:35:12 -0500

    added state hook for cards

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,47 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card'
 
-class CardContainer extends React.Component {
-	render() {
-		const cards = [{color: 'green',
+function CardContainer() {
+	const cards = {0: [{color: 'green',
 	                    headerText: 'Yes',
 	                    bodyHeader: 'It was',
 	                    bodyText: 'infuriating.',
-	                    buttonText: 'Once.'}, 
+	                    buttonText: 'Once.',
+	                    targetCardSet : 1}, 
 	                   {color: 'teal',
 	                    headerText: 'Yes',
 	                    bodyHeader: 'I know',
 	                    bodyText: 'the patience of revenge.',
-	                    buttonText: 'Many times.'}, 
+	                    buttonText: 'Many times.',
+	                    targetCardSet : 1}, 
 	                   {color: 'blue',
-                        headerText: 'No',
-                        bodyHeader: 'But,',
+	                    headerText: 'No',
+	                    bodyHeader: 'But,',
 	                    bodyText: 'somehow.',
-	                    buttonText: "I'm grumpy"}];
+	                    buttonText: "I'm grumpy",
+	                    targetCardSet : 1}],
+	               1: [{color: 'green',
+	                     headerText: 'Forgivness',
+	                     bodyHeader: 'Might',
+	                     bodyText: 'be.',
+	                     buttonText: 'Best.',
+	                     targetCardSet : 0}, 
+	                    {color: 'teal',
+	                     headerText: 'Infuriating',
+	                     bodyHeader: "Doesn't quite",
+	                     bodyText: 'cover the',
+	                     buttonText: 'Feeling.',
+	                     targetCardSet : 0}]};
 
-	    const onceOption = [{color: 'green',
-	                    headerText: 'Forgivness',
-	                    bodyHeader: 'Might',
-	                    bodyText: 'be.',
-	                    buttonText: 'Best.'}, 
-	                   {color: 'teal',
-	                    headerText: 'Infuriating',
-	                    bodyHeader: "Doesn't quite",
-	                    bodyText: 'cover the',
-	                    buttonText: 'Feeling.'}];
-		return(
-			<div className="cards-container flex-row flex-justify-center">
-				{cards.map((value, index) => {
-					return <Card key={index} 
-					             color={value.color} 
-					             headerText={value.headerText} 
-					             buttonText={value.buttonText} 
-					             bodyHeader={value.bodyHeader}
-					             bodyText={value.bodyText}/>
-				})}
-			</div>
-		)
-	}
+    const onceOption = [];
+    const [displayedCard, setCard] = useState(0);
+	return(
+		<div className="cards-container flex-row flex-justify-center">
+			{cards[displayedCard].map((value, index) => {
+				return <Card key={index} 
+				             color={value.color} 
+				             headerText={value.headerText} 
+				             buttonText={value.buttonText} 
+				             bodyHeader={value.bodyHeader}
+				             bodyText={value.bodyText}/>
+			})}
+		</div>
+	)
 }
 
 export default CardContainer;
