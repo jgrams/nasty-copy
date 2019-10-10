0a44bfe4c6c1ec4569d1060bbad7fc8c3505aff8
jgrams <tynanjk@gmail.com>
2019-10-10 18:12:10 -0500

    port handling

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
-const port = 8080
+const port = normalizePortS(process.env.Port || 8080)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Express server at port ${port}!`))
