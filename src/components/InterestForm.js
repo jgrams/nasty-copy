8a4bcc4fef7b06096e40497e55a28096cd4c28e6
jgrams <tynanjk@gmail.com>
2019-10-11 17:35:51 -0500

    prevent default

--- src/components/InterestForm.js
+++ src/components/InterestForm.js
@@ -1,25 +1,27 @@
 import React from 'react'
 
-class InterestForm extends React.Component {
-	render() {
-		return(
-			<form className="interest-form column align-center" onSubmit="return false">
-				<label>
-					Your Email:
-					<input type="text" />
-				</label>
-				<label>
-					What's Wrong:
-					<input type="text" />
-				</label>
-				<label>
-					I Promise To Pay Money:
-					<input type="checkbox" />
-				</label>
-				<input type="submit" action="/api/post" method="POST" value="Submit" />
-			</form>
-		)
+function InterestForm () {
+	function handleSubmit (e) {
+		e.preventDefault()
 	}
+
+	return(
+		<form className="interest-form column align-center">
+			<label>
+				Your Email:
+				<input type="text" />
+			</label>
+			<label>
+				What's Wrong:
+				<input type="text" />
+			</label>
+			<label>
+				I Promise To Pay Money:
+				<input type="checkbox" />
+			</label>
+			<input type="submit" onClick={handleSubmit} value="Submit" />
+		</form>
+	)
 }
 
 export default InterestForm;
