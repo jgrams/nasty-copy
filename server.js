7470236cf66853f34320c32a75fba1609345d52b
jgrams <tynanjk@gmail.com>
2019-10-10 19:10:12 -0500

    apps communicating

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
-const port = normalizePortS(process.env.Port || 8080)
+const port = process.env.Port || 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/post', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Express server at port ${port}!`))
 
 
