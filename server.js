8990917c1a53cd62b3a93af4a8f82ccb16ee9e31
jgrams <tynanjk@gmail.com>
2019-10-11 18:49:42 -0500

    debugging post going to wrong post

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path')
 const app = express()
 const port = process.env.Port || 8080
 
-app.post('/api/post', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public', 'index.html')))
+app.post('/api/create', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Express server at port ${port}!`))
 
 
--- src/components/InterestForm.js
+++ src/components/InterestForm.js
@@ -3,23 +3,30 @@ import React from 'react'
 function InterestForm () {
 	function handleSubmit (e) {
 		e.preventDefault()
+		const data = new FormData(e.target) 
+		var xhr = new XMLHttpRequest()
+		xhr.open('POST', '/api/create', true)
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+		xhr.onreadystatechange = function() {
+			if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+
+			}
+		}
+		xhr.send(JSON.stringify(data))
 	}
 
 	return(
-		<form className="interest-form column align-center">
-			<label>
-				Your Email:
-				<input type="text" />
-			</label>
-			<label>
-				What's Wrong:
-				<input type="text" />
-			</label>
-			<label>
-				I Promise To Pay Money:
-				<input type="checkbox" />
-			</label>
-			<input type="submit" onClick={handleSubmit} value="Submit" />
+		<form onSubmit={handleSubmit} className="interest-form column align-center">
+			<label htmlFor="email">Your Email:</label>
+			<input name="email" type="email" />
+			
+			<label htmlFor="wrong">What's Wrong:</label>
+			<input name="wrong" type="text" />
+			
+			<label htmlFor="willpay" >I Promise To Pay Money:</label>
+			<input name="willpay" type="checkbox" />
+			
+			<button>Submit</button>
 		</form>
 	)
 }
