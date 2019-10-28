ad074687d2494ec06300b8c4c0cc31812dbf6de6
jgrams <tynanjk@gmail.com>
2019-10-28 17:32:01 -0500

    cleanui database migration

--- server/server.js
+++ server/server.js
@@ -3,21 +3,19 @@ const bodyParser = require('body-parser')
 const path = require('path')
 var mysql = require('@mysql/xdevapi');
 var config = require('../database/config/dev.json')
-var databaseCreation = require('../database/migrations/createdatabase.json')
+var createDatabase = require('../database/migrations/createdatabase.json')
 
 const app = express()
 const port = process.env.Port || 8080
 
-mysql.getSession(config)
-     .then(session => {
-     	session.sql(databaseCreation.create)
-     	       .execute()
-     	session.sql(databaseCreation.submissions)
-     	       .execute()
-     	console.log(session.inspect());
-     })
-
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
+mysql.getSession(config)
+     .then(session => {
+     	session.sql(createDatabase.create)
+     	       .execute()
+     	session.sql(createDatabase.submissions)
+     	       .execute();
+     })
 app.post('/api/create', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Express server at port ${port}!`))
