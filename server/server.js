4e02a197990460480a28fe415cd6a06f2b8f85fa
jgrams <tynanjk@gmail.com>
2020-02-28 18:03:30 -0600

    error handling and updating server to use AWS env vars

--- server/server.js
+++ server/server.js
@@ -1,43 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const mysql = require('@mysql/xdevapi');
-const config = require('../database/config/dev.json')
-const createDatabase = require('../database/migrations/createdatabase.json')
+const mysql = require('mysql')
 const app = express()
 const port = process.env.Port || 8080
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
+var pool = mysql.createPool({
+	host     : process.env.RDS_HOSTNAME,
+	user     : process.env.RDS_USERNAME,
+	password : process.env.RDS_PASSWORD,
+	port     : process.env.RDS_PORT,
+	database : process.env.RDS_DB_NAME
+})
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../build', 'index.html')))
-
-
-mysql.getSession(config)
-     .then(session => {
-     	session.sql(createDatabase.create)
-     	       .execute()
-     	session.sql(createDatabase.submissions)
-     	       .execute();
-     console.log(`Database created.`)
-     })
-
-
-app.post('/api/create', (req, res) => { 
-		console.log(req.body);
-		mysql
-		  .getSession(config)
-		  .then(function (session) {
-		    // Accessing an existing table
-		    myTable = session.getSchema('nastycopy').getTable('submissions');
-
-		    // Insert SQL Table data
-		    return myTable
-		      .insert(['email', 'grievance', 'paywilling'])
-		      .values([req.body.email, req.body.wrong, req.body.willpay])
-		      .execute()
-		  });
-         res.send('Post Successful!');
-    });
+app.post('/api/create', (req, res, next) => { 
+		var values = {email: req.body.email, 
+		              wrong: req.body.wrong,
+		              willpay: req.body.willpay};
+		// mysql library escapes when passed in as parameter                
+	  	pool.query('INSERT INTO submission SET ?', 
+	  		       values, 
+	  		       function (error, results, fields) {
+	  		           if (error) {
+	  		             next(error)
+	  	           	   } else {
+	  	           	   	 res.send('Thanks for your submission.')
+	  	           	   }
+	  	           }
+	  	);
+})
 
 app.listen(port, () => console.log(`Express server at port ${port}!`))
